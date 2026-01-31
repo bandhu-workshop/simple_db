94632dd6b1075d655dc19871ec9cbac02ed47196
dinabandhu50 <beheradinabandhu50@gmail.com>
2026-01-31 15:00:21 +0530

    Refactor config.py: comment out PostgreSQL URL configuration for clarity

--- src/simple_db/config.py
+++ src/simple_db/config.py
@@ -4,9 +4,9 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv(
-        "POSTGRESS_URL", "postgresql+psycopg://admin:admin123@localhost:5432/fastapi_db"
-    )
+    # DATABASE_URL: str = os.getenv(
+    #     "POSTGRESS_URL", "postgresql+psycopg://admin:admin123@localhost:5432/fastapi_db"
+    # )
     DATABASE_URL: str = os.getenv("SQLITE_URL", "sqlite:///./database.db")
     DEBUG: bool = True
 
